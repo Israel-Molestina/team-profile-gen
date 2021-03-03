b24c41c14b9127932fc5aed1875b58b540b6aea1
Israel Molestina <molestinaisrael@gmail.com>
2021-03-02 23:29:59 -0500

    created array of prompts for user

--- index.js
+++ index.js
@@ -3,8 +3,72 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const Employee = require('./lib/Employee')
 
-// init new employee
-const employee = new Employee();
+// array of questions for user input
+const questions = [
+    {
+        type: 'list',
+        message: "What role will this employee be fulfilling?",
+        choices: [ "Manager", "Engineer", "Intern" ],
+        name: 'role',
+    },
+    {
+        type: 'input',
+        message: "Please enter team members name",
+        name: 'name',
+        validate: function validateName(name){
+            return name !== '';
+        },
+    },
+    {
+        type: 'input',
+        message: "Please enter team members id",
+        name: 'id',
+        validate: function validateId(id){
+            return id !== '';
+        },
+    },
+    {
+        type: 'input',
+        message: "Please enter team members email",
+        name: 'email',
+        validate: function validateEmail(email){
+            return email !== '';
+        },
+    },
+    {
+        type: 'input',
+        message: "Please input Manager's office number",
+        name: 'officeNum',
+        when: (answers) => answers.role === Manager,
+        validate: function validateOfficeNum(officeNum){
+            return officeNum !== '';
+        },
+    },
+    {
+        type: 'input',
+        message: "Please input Engineer's GitHub",
+        name: 'gitHub',
+        when: (answers) => answers.role === Engineer,
+        validate: function validateGithub(gitHub){
+            return gitHub !== '';
+        },
+    },
+    {
+        type: 'input',
+        message: "Please input Intern's school",
+        name: 'school',
+        when: (answers) => answers.role === Intern,
+        validate: function validateSchool(school){
+            return school !== '';
+        },
+    },
+];
 
-// start prompts for new employee
-employee.getName();
+function init() {
+    inquirer.prompt([...questions])
+    .then ((answers) => {
+
+    })
+}
+
+init();
--- tests/employee.test.js
+++ tests/employee.test.js
@@ -2,8 +2,7 @@ const Employee = require('../lib/Employee');
 
 describe("Employee name", () => {
     it("adds the name user inputs to variable name", () => {
-        const employee = new Employee("Izy");
 
-        expect(employee.name).toEqual("Izy")
+        expect(new Employee().getName("Izy")).toEqual("Izy")
     });
 });
